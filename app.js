5418ba5c4fabf12b2f2269000b61db5fc9f75b0e
ashimol <ashithaea@gmail.com>
2025-02-03 19:23:17 +0530

    debugging

--- app.js
+++ app.js
@@ -13,21 +13,22 @@ const cors = require("cors");
 const db = require("./config/db");
 db();
   
-const allowedOrigins = ['http://localhost:3000', 'https://ashitha.live'];
+const allowedOrigins = ['http://localhost:3000', 'https://ashitha.live', 'http://localhost'];
 
 app.use(cors({
     origin: function(origin, callback) {
-
+        
         if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
+        
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
         }
-        return callback(null, true);
     },
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type']
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 
